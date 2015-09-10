42d8b13a7811f04244c6163db1198faec56ec353
vkd <vkd.castle@gmail.com>
2015-09-10 17:39:35 +0300

    fix fetch in "sophia" folder

--- install_lib.sh
+++ install_lib.sh
@@ -8,12 +8,13 @@ sophia_repo='https://github.com/pmwkaa/sophia.git'
 mkdir -p lib
 cd lib
 if [ -d "sophia" ]; then
+    cd sophia
     git fetch $sophia_repo
 else
     git clone $sophia_repo
+    cd sophia
 fi
 
-cd sophia
 make
 sudo cp -v db/libsophia.a "$prefix"
 for file in db/libsophia.so*; do
